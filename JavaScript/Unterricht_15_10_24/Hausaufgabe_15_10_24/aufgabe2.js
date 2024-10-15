3e348339e04907ebf2b75268c7a54ce3e0114aab
Alf <gruber.1992@gmx.de>
2024-10-15 14:14:14 +0200

    Dateien für die Hausaufgabe vom 15.10.2024 in Repo hochgeladen.

---
+++ JavaScript/Unterricht_15_10_24/Hausaufgabe_15_10_24/aufgabe2.js
@@ -0,0 +1,13 @@
+function Ziffernsumme(number) {
+    let sum = 0;
+
+    do {
+        sum += number % 10;
+        number = Math.floor(number / 10);
+    } while (number > 0);
+
+    return sum;
+}
+
+const ergebnisSum = Ziffernsumme(248);
+console.log("Die Ziffernsumme von 248 ist: " + ergebnisSum)
