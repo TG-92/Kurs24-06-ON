6dc04ec59ec9b9f6164b71b6d938bf4adab509f9
Alf <gruber.1992@gmx.de>
2024-10-23 16:45:49 +0200

    CustomTextField sauber eingebunden und eine Ausgabe auf der Website eingebaut.

--- REACT/Unterricht_23_10_24_Vertretung_T/Hausaufgabe/react-aws-24-06/src/App.js
+++ REACT/Unterricht_23_10_24_Vertretung_T/Hausaufgabe/react-aws-24-06/src/App.js
@@ -1,4 +1,4 @@
-// import logo from "./logo.svg";
+import React, { useState } from "react";  // useState hinzufügen
 import "./App.css";
 import Navbar from "./components/layout/navbar/Navbar";
 import Content from "./components/layout/content/Content";
@@ -7,20 +7,30 @@ import StandardBtn from "./components/common/standard-btn/StandardBtn";
 import CustomTextField from "./components/common/customtxtfield/CustomTxtField";
 
 function App() {
+  // useState-Hook, um den Zustand für das Eingabefeld zu verwalten
+  var [inputValue, setInputValue] = useState('');
+
+  // Funktion zur Verarbeitung der Eingabe
+  var handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <div className="main-container">
       <Navbar />
       <Content>
         <div>Hello from Earth</div>
         <StandardBtn>IT</StandardBtn>
-        <CustomTextField />
+        <CustomTextField 
+          label="Tim"
+          placeholder="Gib deinen Namen ein"
+          onChange={handleInputChange}  // handleInputChange Funktion
+        />
+        <p>Eingegebener Text: {inputValue}</p>  {/* Anzeige des eingegebenen Textes */}
       </Content>
       <Footer />
     </div>
   );
 }
 
-
-
 export default App;
-
