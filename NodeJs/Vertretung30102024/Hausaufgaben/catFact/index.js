14330638bb38582b9b06d06a2e7a0c56da188719
Alf <gruber.1992@gmx.de>
2024-10-30 18:19:46 +0100

    Struktur mit Prettier verschönert.

--- NodeJs/Vertretung30102024/Hausaufgaben/catFact/index.js
+++ NodeJs/Vertretung30102024/Hausaufgaben/catFact/index.js
@@ -1,53 +1,57 @@
-import inquirer from 'inquirer';
-import boxen from 'boxen';
-import catFacts from 'cat-facts';
-import chalk from 'chalk';
+import inquirer from "inquirer";
+import boxen from "boxen";
+import catFacts from "cat-facts";
+import chalk from "chalk";
 
-// Username Abfragen 
+// Username Abfragen
 
 async function Usernamecheck() {
-    const answer = await inquirer.prompt({type: "input", name: "username", message: "Wie heißt Du?",})
-    console.log (answer.username)
-    
-    return answer.username; // Benutzername wird ausgegeben.
+  const answer = await inquirer.prompt({
+    type: "input",
+    name: "username",
+    message: "Wie heißt Du?",
+  });
+  console.log(answer.username);
+
+  return answer.username; // Benutzername wird ausgegeben.
 }
 
 // Erstellen der Cat-Facts Funktion
 
 async function showCatFact() {
-
-    const fact = catFacts.random(); // Zufälliger Katzenfakt wird aufgerufen
-    const CatBox = boxen(chalk.red(fact), { padding: 1, // Mit chalk wird aufgerufen und das Zitat Rot angezeigt.
-    margin: 1, borderStyle: 'double' });
-    console.log(CatBox); // Zitat in einer Box anzeigen
+  const fact = catFacts.random(); // Zufälliger Katzenfakt wird aufgerufen
+  const CatBox = boxen(chalk.red(fact), {
+    padding: 1, // Mit chalk wird aufgerufen und das Zitat Rot angezeigt.
+    margin: 1,
+    borderStyle: "double",
+  });
+  console.log(CatBox); // Zitat in einer Box anzeigen
 }
 
 async function askForNewQuote() {
-    const answers = await inquirer.prompt([
+  const answers = await inquirer.prompt([
     {
-    type: 'confirm',
-    name: 'showCatFacts',
-    message: 'Bock auf n`Cat Fact?',
-    default: true,
+      type: "confirm",
+      name: "showCatFacts",
+      message: "Bock auf n`Cat Fact?",
+      default: true,
     },
-    ]);
-    // 1. Wenn der Benutzer "Ja" wählt, zeige ein Zitat an
-    // 2. Wenn der Benutzer "Nein" wählt, beende das Programm
-    if (answers.showCatFacts) {
+  ]);
+  // 1. Wenn der Benutzer "Ja" wählt, zeige ein Zitat an
+  // 2. Wenn der Benutzer "Nein" wählt, beende das Programm test
+  if (answers.showCatFacts) {
     showCatFact(); // Zitat anzeigen
     askForNewQuote();
-    } else {
-    console.log(chalk.yellow('Bis zum Miaoschsten mal <3'));
+  } else {
+    console.log(chalk.yellow("Bis zum Miaoschsten mal <3"));
     process.exit(0); // Programm beenden
- }
-    }
+  }
+}
 
 async function main() {
-
-    const username = await Usernamecheck();
-    console.log("Hallo " + username + ", Willkommen im CatFact-Tool!");
-    await askForNewQuote();
-    
+  const username = await Usernamecheck();
+  console.log("Hallo " + username + ", Willkommen im CatFact-Tool!");
+  await askForNewQuote();
 }
 
 main();
